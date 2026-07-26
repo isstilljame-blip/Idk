982e211c8d2504b77f97c6e8f50ef55a2dcc86fc
isstilljame-blip <isstilljame@gmail.com>
2026-07-26 20:38:02 +0630

    Add files via upload

---
+++ jackpot2.lua
@@ -0,0 +1,33 @@
+-- UhhhhhhReanim/Modules/biast.lua
+local modules = {}
+table.insert(modules, function()
+    local m = {}
+    m.ModuleType  = "DANCE"
+    m.Name        = "Hold on wait I just hit A Black flash"
+    m.Description = "ay ay ay ay ay"
+    m.Assets = {"jackpot2.anim", "jackpot2.mp3"}
+    m.Config = function(parent)
+        Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+    end
+    m.SaveConfig = function() return {} end
+    m.LoadConfig  = function(save) end
+    local animator = nil
+    local start    = 0
+    m.Init = function(figure)
+        SetOverrideDanceMusic(AssetGetContentId("jackpot2.mp3"), "banging", 0.8, NumberRange.new(0, 45.5))
+        start           = os.clock()
+        animator        = AnimLib.Animator.new()
+        animator.rig    = figure
+        animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("jackpot2.anim"))
+        animator.looped = true
+        animator.speed  = 1
+    end
+    m.Update = function(dt, figure)
+        animator:Step(os.clock() - start)
+    end
+    m.Destroy = function(figure)
+        animator = nil
+    end
+    return m
+end)
+return modules
